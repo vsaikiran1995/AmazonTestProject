e5efb08ddc0906b12690359a970456cc92698a63
vsaikiran1995 <vsaikiran1995@gmail.com>
2023-03-27 18:42:52 +0530

    added screenshot method and close browser statements

--- src/test/AmazonTest.java
+++ src/test/AmazonTest.java
@@ -1,16 +1,21 @@
 package test;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class AmazonTest {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
 
 		WebDriver driver = new ChromeDriver();
@@ -27,9 +32,8 @@ public class AmazonTest {
 
 		List<WebElement> products = driver.findElements(By.xpath("//div[@class='a-section']//h2//span"));
 
-		List<WebElement> productPrice = driver
-				.findElements(By.xpath("//div[@data-component-type='s-search-result']//div[contains(@class,'price')]//span[@class='a-price-whole']"));
-		
+		List<WebElement> productPrice = driver.findElements(By.xpath(
+				"//div[@data-component-type='s-search-result']//div[contains(@class,'price')]//span[@class='a-price-whole']"));
 		List<WebElement> productCurrency = driver
 				.findElements(By.xpath("//div[@class='sg-row']//span[@class='a-price-symbol']"));
 
@@ -39,6 +43,13 @@ public class AmazonTest {
 			System.out.println(productCurrency.get(i).getText() + "" + productPrice.get(i).getText());
 
 		}
+		
+		TakesScreenshot ss =  (TakesScreenshot)driver;
+		File ssFile=ss.getScreenshotAs(OutputType.FILE);
+		File fileObj = new File("Amazon.png");
+		FileUtils.copyFile(ssFile, fileObj);
+		
+		driver.close();
 
 	}
 
